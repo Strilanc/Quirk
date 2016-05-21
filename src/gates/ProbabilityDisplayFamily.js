72c6b34df21c50a73bff4b894e7203065230e375
Craig Gidney <craig.gidney@gmail.com>
2016-05-20 22:54:27 -0400

    Fixed chance gate appearing to get darker due to dividers covering too much area

--- src/gates/ProbabilityDisplayFamily.js
+++ src/gates/ProbabilityDisplayFamily.js
@@ -65,11 +65,12 @@ function _paintMultiProbabilityDisplay_grid(args) {
     let n = 1 << args.gate.height;
     let d = h/n;
     painter.fillRect(args.rect, Config.DISPLAY_GATE_BACK_COLOR);
+    let r = args.gate.height - 5;
     painter.trace(tracer => {
         for (let i = 1; i < n; i++) {
             tracer.line(x, y + d*i, x+w, y+d*i);
         }
-    }).thenStroke('lightgray');
+    }).thenStroke('lightgray', r <= 0 ? 1 : 1/r);
     painter.strokeRect(args.rect, 'lightgray');
 }
 
