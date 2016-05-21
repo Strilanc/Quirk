feca3119314c7f82027f2f431b492ba26e9e6845
Craig Gidney <craig.gidney@gmail.com>
2016-05-21 16:12:29 -0400

    Pulled ArithmeticGates.js out of AllGates.js

--- src/gates/AllGates.js
+++ src/gates/AllGates.js
@@ -14,6 +14,7 @@ import {seq, Seq} from "src/base/Seq.js"
 import ShaderPipeline from "src/circuit/ShaderPipeline.js"
 import Shaders from "src/webgl/Shaders.js"
 
+import ArithmeticGates from "src/gates/ArithmeticGates.js"
 import AmplitudeDisplayFamily from "src/gates/AmplitudeDisplayFamily.js"
 import DensityMatrixDisplayFamily from "src/gates/DensityMatrixDisplayFamily.js"
 import ProbabilityDisplayFamily from "src/gates/ProbabilityDisplayFamily.js"
@@ -190,112 +191,6 @@ Gates.HalfTurns = {
         "Maps ON to ON + OFF.\n" +
         "Maps OFF to ON - OFF.")
 };
-
-const INCREMENT_MATRIX_MAKER = span =>
-    Matrix.generate(1<<span, 1<<span, (r, c) => ((r-1) & ((1<<span)-1)) === c ? 1 : 0);
-const DECREMENT_MATRIX_MAKER = span =>
-    Matrix.generate(1<<span, 1<<span, (r, c) => ((r+1) & ((1<<span)-1)) === c ? 1 : 0);
-
-Gates.IncrementFamily = Gate.generateFamily(1, 16, span => Gate.withoutKnownMatrix(
-    "+1",
-    "Increment Gate",
-    "Adds 1 to the little-endian number represented by a block of qubits.").
-    markedAsOnlyPermutingAndPhasing().
-    markedAsStable().
-    withKnownMatrix(span >= 4 ? undefined : INCREMENT_MATRIX_MAKER(span)).
-    withSerializedId("inc" + span).
-    withHeight(span).
-    withCustomShader((val, con, bit) => GateShaders.increment(val, con, bit, span, +1)));
-Gates.DecrementFamily = Gate.generateFamily(1, 16, span => Gate.withoutKnownMatrix(
-    "-1",
-    "Decrement Gate",
-    "Subtracts 1 from the little-endian number represented by a block of qubits.").
-    markedAsOnlyPermutingAndPhasing().
-    markedAsStable().
-    withKnownMatrix(span >= 4 ? undefined : DECREMENT_MATRIX_MAKER(span)).
-    withSerializedId("dec" + span).
-    withHeight(span).
-    withCustomShader((val, con, bit) => GateShaders.increment(val, con, bit, span, -1)));
-
-const ADDITION_MATRIX_MAKER = span => Matrix.generate(1<<span, 1<<span, (r, c) => {
-    let expected = r;
-    let input = c;
-    let sa = Math.floor(span/2);
-    let sb = Math.ceil(span/2);
-    let a = input & ((1 << sa) - 1);
-    let b = input >> sa;
-    b += a;
-    b &= ((1 << sb) - 1);
-    let actual = a + (b << sa);
-    return expected === actual ? 1 : 0;
-});
-const SUBTRACTION_MATRIX_MAKER = span => Matrix.generate(1<<span, 1<<span, (r, c) => {
-    let expected = r;
-    let input = c;
-    let sa = Math.floor(span/2);
-    let sb = Math.ceil(span/2);
-    let a = input & ((1 << sa) - 1);
-    let b = input >> sa;
-    b -= a;
-    b &= ((1 << sb) - 1);
-    let actual = a + (b << sa);
-    return expected === actual ? 1 : 0;
-});
-
-Gates.AdditionFamily = Gate.generateFamily(2, 16, span => Gate.withoutKnownMatrix(
-    "b+=a",
-    "Addition Gate",
-    "Adds a little-endian number into another.").
-    markedAsOnlyPermutingAndPhasing().
-    markedAsStable().
-    withKnownMatrix(span >= 5 ? undefined : ADDITION_MATRIX_MAKER(span)).
-    withSerializedId("add" + span).
-    withCustomDrawer(GatePainting.SECTIONED_DRAWER_MAKER(["a", "b+=a"], [Math.floor(span/2) / span])).
-    withHeight(span).
-    withCustomShader((val, con, bit) => GateShaders.addition(val, con, bit, Math.floor(span/2), Math.ceil(span/2), 1)));
-Gates.SubtractionFamily = Gate.generateFamily(2, 16, span => Gate.withoutKnownMatrix(
-    "b-=a",
-    "Subtraction Gate",
-    "Subtracts a little-endian number from another.").
-    markedAsOnlyPermutingAndPhasing().
-    markedAsStable().
-    withKnownMatrix(span >= 5 ? undefined : SUBTRACTION_MATRIX_MAKER(span)).
-    withSerializedId("sub" + span).
-    withCustomDrawer(GatePainting.SECTIONED_DRAWER_MAKER(["a", "b-=a"], [Math.floor(span/2) / span])).
-    withHeight(span).
-    withCustomShader((val, con, bit) => GateShaders.addition(val, con, bit, Math.floor(span/2), Math.ceil(span/2),-1)));
-
-const COUNTING_MATRIX_MAKER = span =>
-    t => Matrix.generate(1<<span, 1<<span, (r, c) => ((r-Math.floor(t*(1<<span))) & ((1<<span)-1)) === c ? 1 : 0);
-const UNCOUNTING_MATRIX_MAKER = span =>
-    t => Matrix.generate(1<<span, 1<<span, (r, c) => ((r+Math.floor(t*(1<<span))) & ((1<<span)-1)) === c ? 1 : 0);
-
-Gates.CountingFamily = Gate.generateFamily(1, 8, span => Gate.withoutKnownMatrix(
-    "(+1)^⌈t⌉",
-    "Counting Gate",
-    "Adds an increasing little-endian count into a block of qubits.").
-    markedAsOnlyPermutingAndPhasing().
-    markedAsStable().
-    withKnownMatrixFunc(span >= 4 ? undefined : COUNTING_MATRIX_MAKER(span)).
-    withSerializedId("Counting" + span).
-    withCustomDrawer(GatePainting.SQUARE_WAVE_DRAWER_MAKER(0, 1 << span)).
-    withHeight(span).
-    withStableDuration(1.0 / (1<<span)).
-    withCustomShader((val, con, bit, time) => GateShaders.increment(val, con, bit, span,
-        Math.floor(time*(1<<span)))));
-Gates.UncountingFamily = Gate.generateFamily(1, 8, span => Gate.withoutKnownMatrix(
-    "(-1)^⌈t⌉",
-    "Down Counting Gate",
-    "Subtracts an increasing little-endian count from a block of qubits.").
-    markedAsOnlyPermutingAndPhasing().
-    markedAsStable().
-    withKnownMatrixFunc(UNCOUNTING_MATRIX_MAKER(span)).
-    withSerializedId("Uncounting" + span).
-    withCustomDrawer(GatePainting.SQUARE_WAVE_DRAWER_MAKER(0, 1 << span, true)).
-    withHeight(span).
-    withStableDuration(1.0 / (1<<span)).
-    withCustomShader((val, con, bit, time) => GateShaders.increment(val, con, bit, span,
-        -Math.floor(time*(1<<span)))));
 
 const FOURIER_TRANSFORM_MATRIX_MAKER = span =>
     Matrix.generate(1<<span, 1<<span, (r, c) => Complex.polar(Math.pow(0.5, span/2), τ*r*c/(1<<span)));
@@ -746,6 +641,8 @@ Gates.ExperimentalAndImplausible = {
         withCustomShader((val, con, bit) => GateShaders.cycleBits(val, con, bit, span, 1)))
 };
 
+Gates.Arithmetic = ArithmeticGates;
+
 /** @type {!Array<!{hint: !string, gates: !Array<?Gate>}>} */
 Gates.Sets = [
     {
@@ -817,12 +714,12 @@ Gates.Sets = [
     {
         hint: 'Arithmetic',
         gates: [
-            Gates.CountingFamily.ofSize(2),
-            Gates.AdditionFamily.ofSize(4),
-            Gates.IncrementFamily.ofSize(2),
-            Gates.UncountingFamily.ofSize(2),
-            Gates.SubtractionFamily.ofSize(4),
-            Gates.DecrementFamily.ofSize(2)
+            Gates.Arithmetic.CountingFamily.ofSize(2),
+            Gates.Arithmetic.AdditionFamily.ofSize(4),
+            Gates.Arithmetic.IncrementFamily.ofSize(2),
+            Gates.Arithmetic.UncountingFamily.ofSize(2),
+            Gates.Arithmetic.SubtractionFamily.ofSize(4),
+            Gates.Arithmetic.DecrementFamily.ofSize(2)
         ]
     },
     {
@@ -954,12 +851,13 @@ Gates.KnownToSerializer = [
     Gates.OtherZ.Z8i,
     Gates.OtherZ.Z16i,
 
-    ...Gates.IncrementFamily.all,
-    ...Gates.DecrementFamily.all,
-    ...Gates.AdditionFamily.all,
-    ...Gates.SubtractionFamily.all,
-    ...Gates.CountingFamily.all,
-    ...Gates.UncountingFamily.all,
+    ...Gates.Arithmetic.IncrementFamily.all,
+    ...Gates.Arithmetic.DecrementFamily.all,
+    ...Gates.Arithmetic.AdditionFamily.all,
+    ...Gates.Arithmetic.SubtractionFamily.all,
+    ...Gates.Arithmetic.CountingFamily.all,
+    ...Gates.Arithmetic.UncountingFamily.all,
+
     ...Gates.FourierTransformFamily.all,
     ...Gates.PhaseGradientFamily.all,
     ...Gates.PhaseDegradientFamily.all,
