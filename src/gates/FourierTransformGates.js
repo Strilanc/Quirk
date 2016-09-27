775f21bee6013f4b786eff51891c0771380a88fe
Craig Gidney <craig.gidney@gmail.com>
2016-09-26 22:37:59 -0400

    Applied ketShaderPhase to controlledPhaseGradient and phaseGradient

--- src/gates/FourierTransformGates.js
+++ src/gates/FourierTransformGates.js
@@ -3,61 +3,32 @@ import {Config} from "src/Config.js"
 import {Complex} from "src/math/Complex.js"
 import {Gate} from "src/circuit/Gate.js"
 import {GateShaders} from "src/circuit/GateShaders.js"
+import {ketArgs, ketShaderPhase} from "src/circuit/KetShaderUtil.js"
 import {Matrix} from "src/math/Matrix.js"
 import {WglArg} from "src/webgl/WglArg.js"
 import {seq, Seq} from "src/base/Seq.js"
 import {WglConfiguredShader, WglShader} from "src/webgl/WglShader.js"
 
 /**
- * @param {!WglTexture} inputTexture Superposition stored as a grid of amplitudes.
- * @param {!WglTexture} controlTexture 1 at affected amplitudes, 0 at protected amplitudes.
- * @param {!int} qubitIndex Location of the gate.
+ * @param {!CircuitEvalArgs} args
  * @param {!int} qubitSpan Size of the gate.
  * @param {!number=} factor Scaling factor for the applied phases.
  * @returns {!WglConfiguredShader} A configured shader that renders the output superposition (as a grid of amplitudes).
  */
-function controlledPhaseGradient(inputTexture, controlTexture, qubitIndex, qubitSpan, factor=1) {
-    return new WglConfiguredShader(destinationTexture => {
-        CONTROLLED_PHASE_GRADIENT_SHADER.withArgs(
-            WglArg.texture("inputTexture", inputTexture, 0),
-            WglArg.texture("controlTexture", controlTexture, 1),
-            WglArg.float("outputWidth", destinationTexture.width),
-            WglArg.vec2("inputSize", inputTexture.width, inputTexture.height),
-            WglArg.float("qubitIndex", 1 << qubitIndex),
-            WglArg.float("qubitSpan", 1 << (qubitSpan-1)),
-            WglArg.float("factor", factor)
-        ).renderTo(destinationTexture);
-    });
+function controlledPhaseGradient(args, qubitSpan, factor=1) {
+    return CONTROLLED_PHASE_GRADIENT_SHADER.withArgs(
+        ...ketArgs(args, qubitSpan),
+        WglArg.float("factor", factor));
 }
-const CONTROLLED_PHASE_GRADIENT_SHADER = new WglShader(`
-    uniform sampler2D inputTexture;
-    uniform sampler2D controlTexture;
-    uniform float outputWidth;
-    uniform vec2 inputSize;
-    uniform float qubitIndex;
-    uniform float qubitSpan;
-    uniform float factor;
-
-    vec2 toUv(float state) {
-        return (vec2(mod(state, inputSize.x), floor(state / inputSize.x)) + vec2(0.5, 0.5)) / inputSize;
-    }
-
-    void main() {
-        vec2 xy = gl_FragCoord.xy - vec2(0.5, 0.5);
-        float state = xy.y * outputWidth + xy.x;
-        float hold = mod(floor(state / qubitIndex / qubitSpan), 2.0);
-        float step = mod(floor(state / qubitIndex), qubitSpan);
-        float angle = step * factor * 3.141592653589793 / qubitSpan;
-        float c = cos(angle);
-        float s = sin(angle);
-        mat2 rot = mat2(c, s, -s, c);
-
-        vec2 uv = toUv(state);
-        float control = texture2D(controlTexture, uv).x * hold;
-        vec2 inAmp = texture2D(inputTexture, uv).xy;
-        vec2 outAmp = control*(rot*inAmp) + (1.0-control)*inAmp;
-        gl_FragColor = vec4(outAmp.x, outAmp.y, 0.0, 0.0);
-    }`);
+const CONTROLLED_PHASE_GRADIENT_SHADER = ketShaderPhase(
+    `
+        float hold = floor(out_id * 2.0 / span);
+        float step = mod(out_id, span / 2.0);
+        float angle = hold * step * factor * 6.2831853071795864769 / span;
+        return vec2(cos(angle), sin(angle));
+    `,
+    'uniform float factor;',
+    null);
 
 const τ = Math.PI * 2;
 const FOURIER_TRANSFORM_MATRIX_MAKER = span =>
@@ -74,12 +45,7 @@ let swapShaders = n => Seq.range(Math.floor(n/2)).map(i => args => CircuitShader
     args.controlsTexture));
 let gradShaders = (n, factor) => Seq.range(n).
     flatMap(i => [
-        args => controlledPhaseGradient(
-            args.stateTexture,
-            args.controlsTexture,
-            args.row,
-            i + 1,
-            factor),
+        args => controlledPhaseGradient(args, i+1, factor),
         args => GateShaders.matrixOperation(
             args.withRow(args.row + i),
             Matrix.HADAMARD)
--- test/gates/FourierTransformGates.test.js
+++ test/gates/FourierTransformGates.test.js
@@ -1,109 +1,18 @@
 import {Suite, assertThat, assertThrows} from "test/TestUtil.js"
+import {assertThatRandomTestOfCircuitOperationShaderActsLikeMatrix} from "test/CircuitOperationTestUtil.js"
 import {controlledPhaseGradient} from "src/gates/FourierTransformGates.js"
 
-import {CircuitShaders} from "src/circuit/CircuitShaders.js"
 import {Complex} from "src/math/Complex.js"
-import {Controls} from "src/circuit/Controls.js"
-import {Seq} from "src/base/Seq.js"
-import {Shaders} from "src/webgl/Shaders.js"
 import {Matrix} from "src/math/Matrix.js"
-import {WglShader} from "src/webgl/WglShader.js"
-import {WglTexture} from "src/webgl/WglTexture.js"
 
 let suite = new Suite("FourierTransformGates");
 
 suite.webGlTest('controlledPhaseGradient', () => {
-    const π = Math.PI;
-    const c = Math.cos;
-    const s = Math.sin;
-    let input1 = Shaders.data(Seq.repeat([1, 0, 0, 0], 16).flatten().toFloat32Array()).toFloatTexture(4, 4);
-    let noControl = CircuitShaders.controlMask(Controls.NONE).toFloatTexture(4, 4);
-    let bit3On = CircuitShaders.controlMask(Controls.bit(3, true)).toFloatTexture(4, 4);
+    assertThatRandomTestOfCircuitOperationShaderActsLikeMatrix(
+        args => controlledPhaseGradient(args, 3, 1),
+        Matrix.generateDiagonal(8, i => i < 4 ? 1 : Complex.polar(1, (i-4)*Math.PI/4)));
 
-    assertThat(controlledPhaseGradient(input1, noControl, 0, 4).readFloatOutputs(4, 4)).isApproximatelyEqualTo(
-        new Float32Array([
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            c(0), s(0), 0, 0,
-            c(π/8), s(π/8), 0, 0,
-            c(2*π/8), s(2*π/8), 0, 0,
-            c(3*π/8), s(3*π/8), 0, 0,
-            c(4*π/8), s(4*π/8), 0, 0,
-            c(5*π/8), s(5*π/8), 0, 0,
-            c(6*π/8), s(6*π/8), 0, 0,
-            c(7*π/8), s(7*π/8), 0, 0
-        ]), 0.001);
-
-    assertThat(controlledPhaseGradient(input1, noControl, 0, 3).readFloatOutputs(4, 4)).isApproximatelyEqualTo(
-        new Float32Array([
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            c(0), s(0), 0, 0,
-            c(2*π/8), s(2*π/8), 0, 0,
-            c(4*π/8), s(4*π/8), 0, 0,
-            c(6*π/8), s(6*π/8), 0, 0,
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            c(0), s(0), 0, 0,
-            c(π/4), s(π/4), 0, 0,
-            c(2*π/4), s(2*π/4), 0, 0,
-            c(3*π/4), s(3*π/4), 0, 0
-        ]), 0.001);
-
-    assertThat(controlledPhaseGradient(input1, noControl, 1, 3, -1).readFloatOutputs(4, 4)).isApproximatelyEqualTo(
-        new Float32Array([
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            c(-0), s(-0), 0, 0,
-            c(-0), s(-0), 0, 0,
-            c(-π/4), s(-π/4), 0, 0,
-            c(-π/4), s(-π/4), 0, 0,
-            c(-2*π/4), s(-2*π/4), 0, 0,
-            c(-2*π/4), s(-2*π/4), 0, 0,
-            c(-3*π/4), s(-3*π/4), 0, 0,
-            c(-3*π/4), s(-3*π/4), 0, 0
-        ]), 0.001);
-
-    assertThat(controlledPhaseGradient(input1, bit3On, 0, 3).readFloatOutputs(4, 4)).isApproximatelyEqualTo(
-        new Float32Array([
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            1, 0, 0, 0,
-            c(0), s(0), 0, 0,
-            c(π/4), s(π/4), 0, 0,
-            c(2*π/4), s(2*π/4), 0, 0,
-            c(3*π/4), s(3*π/4), 0, 0
-        ]), 0.001);
-
-    let input2 = Shaders.data(Seq.range(16*4).toFloat32Array()).toFloatTexture(4, 4);
-    assertThat(controlledPhaseGradient(input2, noControl, 0, 4).readFloatOutputs(4, 4)).isApproximatelyEqualTo(
-        Seq.range(16).
-            map(i => new Complex(i*4, i*4+1).times(Complex.polar(1, Math.PI*Math.max(0, (i-8))/8))).
-            flatMap(c => [c.real, c.imag, 0, 0]).
-            toFloat32Array(), 0.01);
+    assertThatRandomTestOfCircuitOperationShaderActsLikeMatrix(
+        args => controlledPhaseGradient(args, 4, -1),
+        Matrix.generateDiagonal(16, i => i < 8 ? 1 : Complex.polar(1, -(i-8)*Math.PI/8)));
 });
