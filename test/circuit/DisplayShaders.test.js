6fa52eebe28ee01d804929b928ecb266ad4a9841
Craig Gidney <craig.gidney@gmail.com>
2016-10-05 21:48:34 -0400

    Moved amplitudesToDensities into the density matrix display code

--- test/circuit/DisplayShaders.test.js
+++ test/circuit/DisplayShaders.test.js
@@ -31,27 +31,3 @@ suite.webGlTest("amplitudesToProbabilities", () => {
         1/256, 0, 0, 0
     ]));
 });
-
-suite.webGlTest("amplitudesToDensities", () => {
-    let s = Math.sqrt(0.5);
-    let inp = Shaders.data(new Float32Array([
-        s,0,0,0,
-        0,0,0,0,
-        0,0,0,0,
-        s,0,0,0
-    ])).toFloatTexture(2, 2);
-
-    assertThat(DisplayShaders.amplitudesToDensities(inp, 1).readFloatOutputs(4, 2)).isApproximatelyEqualTo(new Float32Array([
-        0.5,0,0,0, 0,0,0,0,
-        0,0,0,0,   0,0,0,0,
-
-        0,0,0,0, 0,0,0,0,
-        0,0,0,0,   0.5,0,0,0
-    ]));
-    assertThat(DisplayShaders.amplitudesToDensities(inp, 2).readFloatOutputs(4, 4)).isApproximatelyEqualTo(new Float32Array([
-        0.5,0,0,0, 0,0,0,0, 0,0,0,0, 0.5,0,0,0,
-        0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
-        0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
-        0.5,0,0,0, 0,0,0,0, 0,0,0,0, 0.5,0,0,0
-    ]));
-});
