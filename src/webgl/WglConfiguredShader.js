6c2dded1440afeaa81e6ca06c912c1e399b90652
Craig Gidney <craig.gidney@gmail.com>
2016-10-09 16:59:46 -0400

    Added input-type-specific methods to WglConfiguredShader

--- src/webgl/WglConfiguredShader.js
+++ src/webgl/WglConfiguredShader.js
@@ -1,6 +1,15 @@
 import {Matrix} from "src/math/Matrix.js"
 import {WglTexture} from "src/webgl/WglTexture.js"
 
+/** @type {undefined|!ShaderValueCoder} */
+let workingShaderCoder = undefined;
+/**
+ * @param {!ShaderValueCoder} futureWorkingShaderCoder
+ */
+function provideWorkingShaderCoderToWglConfiguredShader(futureWorkingShaderCoder) {
+    workingShaderCoder = futureWorkingShaderCoder;
+}
+
 /**
  * A shader with all its arguments provided, ready to render to a texture.
  */
@@ -81,6 +90,92 @@ class WglConfiguredShader {
             texture.ensureDeinitialized();
         }
     }
+
+    /**
+     * @param {!int} order
+     * @returns {!Float32Array}
+     */
+    readVec2Outputs(order) {
+        return workingShaderCoder.readVec2Data(this, order);
+    }
+
+    /**
+     * @param {!int} order
+     * @returns {!Matrix}
+     */
+    readVec2OutputsAsKet(order) {
+        return new Matrix(1, 1 << order, this.readVec2Outputs(order));
+    }
+
+    /**
+     * @param {!int} order
+     * @returns {!Uint8Array}
+     */
+    readBoolOutputs(order) {
+        let texture = this.toBoolTexture(order);
+        try {
+            let result = texture.readPixels();
+            for (let i = 0; i < result.length; i++) {
+                result[i] &= 1;
+            }
+            return result;
+        } finally {
+            texture.ensureDeinitialized();
+        }
+    }
+
+    /**
+     * @param {!int} order
+     * @returns {!Float32Array}
+     */
+    readVec4Outputs(order) {
+        return workingShaderCoder.readVec4Data(this, order);
+    }
+
+    /**
+     * @param {!int} order
+     * @returns {!WglTexture}
+     */
+    toVec2Texture(order) {
+        order += workingShaderCoder.vec2Overhead;
+        let width = 1 << Math.ceil(order / 2);
+        let height = 1 << Math.floor(order / 2);
+
+        let texture = new WglTexture(width, height, workingShaderCoder.vecPixelType);
+        this.renderTo(texture);
+        return texture;
+    }
+
+    /**
+     * @param {!int} order
+     * @returns {!WglTexture}
+     */
+    toVec4Texture(order) {
+        order += workingShaderCoder.vec4Overhead;
+        let width = 1 << Math.ceil(order / 2);
+        let height = 1 << Math.floor(order / 2);
+        if (width < 4 ) {
+            width <<= 1;
+            height >>= 1;
+        }
+
+        let texture = new WglTexture(width, height, workingShaderCoder.vecPixelType);
+        this.renderTo(texture);
+        return texture;
+    }
+
+    /**
+     * @param {!int} order
+     * @returns {!WglTexture}
+     */
+    toBoolTexture(order) {
+        let width = 1 << Math.ceil(order / 2);
+        let height = 1 << Math.floor(order / 2);
+
+        let texture = new WglTexture(width, height, WebGLRenderingContext.UNSIGNED_BYTE);
+        this.renderTo(texture);
+        return texture;
+    }
 }
 
-export {WglConfiguredShader}
+export {WglConfiguredShader, provideWorkingShaderCoderToWglConfiguredShader}
