2b564a9f86b2a34b96926b076676df4ead0fae50
Craig Gidney <craig.gidney@gmail.com>
2017-04-23 15:12:01 -0700

    Refactored MultiplicationGates.js to use GateBuilder

--- src/gates/MultiplicationGates.js
+++ src/gates/MultiplicationGates.js
@@ -37,27 +37,25 @@ const INVERSE_MULTIPLICATION_SHADER = ketShaderPermute(
         return mod(out_id * input_a, span);
     `);
 
-MultiplicationGates.TimesAFamily = Gate.generateFamily(1, 16, span => Gate.withoutKnownMatrix(
-    "×A",
-    "Multiplication Gate",
-    "Multiplies the target by input A.\n" +
+MultiplicationGates.TimesAFamily = Gate.buildFamily(1, 16, (span, builder) => builder.
+    setSerializedId("*A" + span).
+    setSymbol("×A").
+    setTitle("Multiplication Gate").
+    setBlurb("Multiplies the target by input A.\n" +
         "No effect if the multiplication would be irreversible.").
-    withSerializedId("*A" + span).
-    withHeight(span).
-    withRequiredContextKeys("Input Range A").
-    withKnownPermutation((x, a) => modularMultiply(x, a, 1<<span)).
-    withCustomShader(ctx => MULTIPLICATION_SHADER.withArgs(...ketArgs(ctx, span, ['A']))));
+    setRequiredContextKeys("Input Range A").
+    setActualEffectToShaderProvider(ctx => MULTIPLICATION_SHADER.withArgs(...ketArgs(ctx, span, ['A']))).
+    setKnownEffectToParametrizedPermutation((x, a) => modularMultiply(x, a, 1<<span)));
 
-MultiplicationGates.TimesAInverseFamily = Gate.generateFamily(1, 16, span => Gate.withoutKnownMatrix(
-    "×A^-1",
-    "Inverse Multiplication Gate",
-    "Inverse-multiplies the target by input A (modulo 2^n).\n" +
+MultiplicationGates.TimesAInverseFamily = Gate.buildFamily(1, 16, (span, builder) => builder.
+    setSerializedId("/A" + span).
+    setSymbol("×A^-1").
+    setTitle("Inverse Multiplication Gate").
+    setBlurb("Inverse-multiplies the target by input A (modulo 2^n).\n" +
         "No effect if the multiplication would be irreversible.").
-    withSerializedId("/A" + span).
-    withHeight(span).
-    withRequiredContextKeys("Input Range A").
-    withKnownPermutation((x, a) => modularUnmultiply(x, a, 1<<span)).
-    withCustomShader(ctx => INVERSE_MULTIPLICATION_SHADER.withArgs(...ketArgs(ctx, span, ['A']))));
+    setRequiredContextKeys("Input Range A").
+    setKnownEffectToParametrizedPermutation((x, a) => modularUnmultiply(x, a, 1<<span)).
+    setActualEffectToShaderProvider(ctx => INVERSE_MULTIPLICATION_SHADER.withArgs(...ketArgs(ctx, span, ['A']))));
 
 MultiplicationGates.all = [
     ...MultiplicationGates.TimesAFamily.all,
