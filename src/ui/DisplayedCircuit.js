38066c961623d0dd22df50c70b41a2c1c44cc141
Craig Gidney <craig.gidney@gmail.com>
2017-04-25 01:53:42 -0700

    Fix label sizing on output superposition display

--- src/ui/DisplayedCircuit.js
+++ src/ui/DisplayedCircuit.js
@@ -1381,20 +1381,20 @@ function _drawLabelsReasonablyFast(painter, dy, n, labeller, boundingWidth) {
         painter.ctx.measureText(labeller(0)).width,
         painter.ctx.measureText(labeller(n-1)).width);
     let h = ctx.measureText("0").width * 2.5;
-    let scale = Math.min(Math.min(boundingWidth / w, dy / h), 1);
+    let scale = Math.min(Math.min((boundingWidth-2) / w, dy / h), 1);
 
     // Row labels.
     let step = dy/scale;
-    let pad = 2/scale;
+    let pad = 1/scale;
     ctx.scale(scale, scale);
     ctx.translate(0, dy*0.5/scale - h*0.5);
     ctx.fillStyle = 'lightgray';
     if (h < step*0.95) {
         for (let i = 0; i < n; i++) {
-            ctx.fillRect(0, step * i, w + 2 * pad, h);
+            ctx.fillRect(0, step*i, w + 2*pad, h);
         }
     } else {
-        ctx.fillRect(0, 0, w + 2 * pad, h*n);
+        ctx.fillRect(0, 0, w + 2*pad, step*n);
     }
     ctx.fillStyle = 'black';
     for (let i = 0; i < n; i++) {
