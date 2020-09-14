a4eca43156fdcffc13d7eefffd1dba6b95df1995
Craig Gidney <craig.gidney@gmail.com>
2020-09-14 05:29:34 -0300

    Screenshot circuit

--- PuppeteerScreenshotCircuit.js
+++ PuppeteerScreenshotCircuit.js
@@ -8,20 +8,16 @@ const puppeteer = require('puppeteer');
         page.on('console', message => console.log(message.text()));
         page.on('pageerror', ({message}) => {
             caughtPageError = true;
-            console.error("Page error bubbled into PuppeteerRunTests.js: " + message);
+            console.error("Page error bubbled into PuppeteerSampleCircuit.js: " + message);
         });
-
         const outDirUrl = 'file:///' + __dirname.split('\\').join('/') + '/out/';
-        await page.goto(outDirUrl + 'test.html#blocking');
-        await page.waitForSelector('#done', {timeout: 5 * 60 * 1000});
-        let anyFailures = await page.evaluate('__any_failures');
-
+        const circuitJson = '{"cols":[["H"],["Bloch"],["Amps1"],[],["Density"],["•","X"],["Chance2"]]}';
+        await page.goto(outDirUrl + 'quirk.html#circuit=' + circuitJson);
+        await page.waitForSelector('#loading-div', {visible: false, timeout: 5 * 1000});
+        await page.screenshot({path: 'screenshot.png'});
         await browser.close();
-        if (anyFailures || caughtPageError) {
-            process.exit(1);
-        }
     } catch (ex) {
-        console.error("Error bubbled up into PuppeteerRunTests.js: " + ex);
+        console.error("Error bubbled up into PuppeteerSampleCircuit.js: " + ex);
         process.exit(1);
     }
 })();
