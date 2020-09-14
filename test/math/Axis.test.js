e734ee4ef40917558146e2cc1292ffe2433afdd1
Craig Gidney <craig.gidney@gmail.com>
2020-09-14 05:03:13 -0300

    Fix intentional test failure

--- test/math/Axis.test.js
+++ test/math/Axis.test.js
@@ -54,7 +54,6 @@ suite.test("parse", () => {
     assertThrows(() => Axis.parse("x*y"));
     assertThrows(() => Axis.parse("x^y"));
     assertThrows(() => Axis.parse("x/y"));
-    assertThat(1).isEqualTo(2);
 
     assertThat(Axis.parse("x")).isEqualTo(new Axis(1, 0, 0));
     assertThat(Axis.parse("y")).isEqualTo(new Axis(0, 1, 0));
